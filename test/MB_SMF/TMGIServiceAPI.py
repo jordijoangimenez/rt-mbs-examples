3fd1a4ab9bbcbbfa781e8fbb44bb816e7340a7ad
Borjis131 <borjainestahernandez@hotmail.com>
2024-11-15 09:33:26 +0000

    Add 5G-MAG license header to the test subproject

--- test/MB_SMF/TMGIServiceAPI.py
+++ test/MB_SMF/TMGIServiceAPI.py
@@ -1,3 +1,12 @@
+"""
+License: 5G-MAG Public License (v1.0)
+Author: Borja Iñesta Hernández
+Copyright: (C) 2024 iTEAM UPV
+For full license terms please see the LICENSE file distributed with this
+program. If this file is missing then the license can be retrieved from
+https://drive.google.com/file/d/1cinCiA778IErENZ3JN52VFW-1ffHpx7Z/view
+"""
+
 import unittest
 import httpcore
 from urllib.parse import urlencode, quote
